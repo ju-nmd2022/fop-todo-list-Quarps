a07459321c6cf2c115f04549850f9c6b87500c93
Quarps <landerzen10@gmail.com>
2023-04-07 20:48:17 +0200

    more backgrounds, item counter

--- toDoList.js
+++ toDoList.js
@@ -45,4 +45,48 @@ function createItem() {
       textElement.style.color = "white";
     }
   });
+
+  // check number of items in listElement is more then 2, changing background
+  function checkListLength() {
+    const items = document.querySelectorAll(".item-div");
+    const backgroundElement = document.querySelector("body");
+    let transitionBackground = (backgroundElement.style.transition = "1.2s");
+    let coverBackground = (backgroundElement.style.backgroundSize = "cover");
+
+    if (items.length === 1) {
+      backgroundElement.style.backgroundImage = "url('img/flowers.jpeg')";
+      coverBackground;
+      transitionBackground;
+    } else if (items.length === 2) {
+      backgroundElement.style.backgroundImage = "url('img/sand.jpeg')";
+      coverBackground;
+      transitionBackground;
+    } else if (items.length === 3) {
+      backgroundElement.style.backgroundImage = "url('img/beach.jpeg')";
+      coverBackground;
+      transitionBackground;
+    } else if (items.length === 4) {
+      backgroundElement.style.backgroundImage = "url('img/nature.jpeg')";
+      coverBackground;
+      transitionBackground;
+    } else if (items.length === 5) {
+      backgroundElement.style.backgroundImage = "url('img/love.jpeg')";
+      coverBackground;
+      transitionBackground;
+    } else if (items.length === 6) {
+      backgroundElement.style.backgroundImage = "url('img/ballon.jpeg')";
+      coverBackground;
+      transitionBackground;
+    } else if (items.length === 7) {
+      backgroundElement.style.backgroundImage = "url('img/winter.jpeg')";
+      coverBackground;
+      transitionBackground;
+    } else {
+      backgroundElement.style.backgroundImage = "url('img/canada.jpeg')";
+      coverBackground;
+      transitionBackground;
+    }
+  }
+
+  checkListLength();
 }
